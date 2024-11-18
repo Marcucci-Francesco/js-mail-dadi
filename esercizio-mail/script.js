065225875361850bb4a7cffaeca643d62a203edb
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-11-18 18:10:45 +0100

    feat: mail exercise completed

--- esercizio-mail/script.js
+++ esercizio-mail/script.js
@@ -4,5 +4,29 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 // Logica
 // Creare un gruppo di mail
 // Impostare una finestra di interazione con l'utente
-// Controllare se l'utente può accedere 
-// Impostare un messaggio di esito
+// Impostare un Flag
+// Impostare un message
+// Creare un circuito di verifica
+// Stampare il messaggio
+
+
+const emails = ['francesco@email.com', 'gino@gmail.com', 'laura@libero.it', 'vanessa@icloud.it', 'renato@mail.com', 'gioia@live.it'];
+let emailUser = prompt('Inserisci la tua email');
+let emailValid = false;
+let message;
+
+for (let i = 0; i < emails.length; i++){
+  if (emails[i] === emailUser){
+    emailValid = true;
+  }
+}
+
+if (!emailValid){
+  message = 'La tua email non è registrata';
+} else {
+  message = 'La tua email è valida';
+}
+
+console.log(message);
+console.log(emailValid);
+
